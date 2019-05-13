086845a391e43cc40b2b5494a98c41b0fe167dd8
Anthony Molina <antonio-molina@att.net>
2019-05-13 06:15:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -16,5 +16,12 @@ puts "-----------"
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
+puts "What move would X like to make (1-9)?"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "what move would O like to make (1-9)?"
 
 
